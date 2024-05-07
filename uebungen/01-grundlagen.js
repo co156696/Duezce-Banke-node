5dcd895e2fc29eafbfcc900e8e1a7a17dd3209e9
Oguzhan Cebeci <156648312+co156696@users.noreply.github.com>
2024-05-07 09:00:33 +0000

    node weiterbearbeitet

--- uebungen/01-grundlagen.js
+++ uebungen/01-grundlagen.js
@@ -17,3 +17,46 @@ console.log("gib das ergebnis der addition von a und b aus.");
 
 console.log(a+b);
 console.log('das ergebnis der addition:' + a+b);
+
+console.log('aufgabe 3');
+console.log('gib das ergebnis der subtraktion, multipikation, division von a und b aus')
+
+console.log("subtraktion:"+ (a - b));
+console.log("multiplikation:"+ (a * b));
+console.log("division:"+ (a / b));
+
+console.log('aufgabe 4');
+console.log('der wert c ei das ergebnis der addition a und b');
+
+let c = a+b
+
+console.log("c hat den wert : " +c);
+
+console.log('aufgabe 5')
+console.log('a und b sind die seitenlängender katheten eines rechtwinkligen dreiecks. bestimme die länge der hypotenuse c')
+
+let cQuadrat = a*a + b*b;
+
+c = Math.sqrt(cQuadrat);
+
+console.log('die hypotenuse ist '+ c +' lang.');
+
+console.log('aufgabe 6');
+console.log('ein kunden legt 100 auf dem sparbuch an. jedes jahr bekommt er 10% zinsen. wie viel bekommt der kunde');
+console.log('nach zwei jahren ausgezahlt. beachte den zinseszinseffekt')
+
+// Anfangskapital
+let p = 100; // Startkapital des Kunden
+
+// Zinssatz
+let r = 0.10; // 10%
+
+// Anzahl der Jahre
+let t = 2; 
+
+let kapitalNachEinemJahr= startkapital * (1 + zinssatz);
+
+// Ausgabe des Endbetrags
+console.log("kapital nach einem Jahr: "+ KapitalNachEinemJahr + "EUR.");
+
+let kapitalNachzweiJahr= KapitalNachEinemJahr * (1 + zinssatz);
